d74664f382b0b87d8b5091ec476a1000682ba851
Bruno Mantovani <bruno.trabalho449243@outlook.com>
2023-02-13 23:32:17 -0300

    Inicio do Projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: String = 'Minhas tarefas';
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(11),
+        Validators.maxLength(11),
+        Validators.required
+      ])]
+    });
+    this.todos.push(new Todo('Passear com o cachorro', false, 1));
+    this.todos.push(new Todo('Ir ao supermercado', false, 2));
+    this.todos.push(new Todo('Cortar o cabelo', true, 3));
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
 }
